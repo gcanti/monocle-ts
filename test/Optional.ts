edc86d5aa4d26e6d8c70e87437912b1045b429b0
gcanti <giulio.canti@gmail.com>
2017-10-09 16:35:47 +0200

    add Optional.fromNullableProp

--- test/Optional.ts
+++ test/Optional.ts
@@ -1,5 +1,5 @@
 import { Option, none, some } from 'fp-ts/lib/Option'
-import { Optional } from '../src'
+import { Optional, Lens } from '../src'
 import { eqOptions as eq } from './helpers'
 
 type A = {
@@ -16,5 +16,47 @@ describe('Optional', () => {
 
   it('set', () => {
     eq(optional.set(2)({ a: some(1) }).a, some(2))
+  })
+
+  it('fromNullableProp', () => {
+    interface Phone {
+      number: string
+    }
+    interface Employment {
+      phone?: Phone
+    }
+    interface Info {
+      employment?: Employment
+    }
+    interface Response {
+      info?: Info
+    }
+
+    const info = Optional.fromNullableProp<Response, Info, 'info'>('info')
+    const employment = Optional.fromNullableProp<Info, Employment, 'employment'>('employment')
+    const phone = Optional.fromNullableProp<Employment, Phone, 'phone'>('phone')
+    const number = Lens.fromProp<Phone, 'number'>('number')
+    const numberFromResponse = info
+      .compose(employment)
+      .compose(phone)
+      .composeLens(number)
+
+    const response1: Response = {
+      info: {
+        employment: {
+          phone: {
+            number: '555-1234'
+          }
+        }
+      }
+    }
+    const response2: Response = {
+      info: {
+        employment: {}
+      }
+    }
+
+    eq(numberFromResponse.getOption(response1), some('555-1234'))
+    eq(numberFromResponse.getOption(response2), none)
   })
 })
