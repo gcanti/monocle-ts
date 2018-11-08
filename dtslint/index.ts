47c5df58fe7f63816c52d343b4709351210c2231
Geert Premereur <bepremeg@users.noreply.github.com>
2018-11-08 07:56:19 +0100

    Removed type-level noise as per code review

--- dtslint/index.ts
+++ dtslint/index.ts
@@ -1,9 +1,11 @@
-import { Lens } from '../src'
+import { Lens, Optional } from '../src'
+import { Option } from 'fp-ts/lib/Option';
 
 interface Person {
   name: string
   age: number
   rememberMe: boolean
+  email: Option<string>
 }
 
 Lens.fromProp<Person, 'name'>('name') // $ExpectType Lens<Person, string>
@@ -19,3 +21,12 @@ Lens.fromProp<Person, 'name'>(['foo'])
 Lens.fromProps<Person>()(['name', 'age']) // $ExpectType Lens<Person, { name: string; age: number; }>
 // $ExpectError
 Lens.fromProps<Person>()(['foo'])
+
+//
+// fromOptionProps
+//
+Optional.fromOptionProp<Person>('email') // $ExpectType Optional<Person, string>
+// $ExpectError
+Optional.fromOptionProp<Person>()('name') // 'name' exists but is not of type Option<any>
+// $ExpectError
+Optional.fromOptionProp<Person>()('foo') // 'foo' is not a property of Person
