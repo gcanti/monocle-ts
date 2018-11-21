93d5a9d8757cdce10981d788399a89ad468ff681
Stéphane Le Dorze <stephane.ledorze@gmail.com>
2018-11-21 16:11:44 +0100

    add Index for NonEmptyArray

---
+++ src/Index/NonEmptyArray.ts
@@ -0,0 +1,6 @@
+import { Index, Optional } from '../index';
+import { NonEmptyArray } from 'fp-ts/lib/NonEmptyArray';
+
+export function indexNonEmptyArray<A = never>(): Index<NonEmptyArray<A>, number, A> {
+  return new Index(i => new Optional(s => s.index(i), a => s => s.updateAt(i, a).getOrElse(s)));
+}
