98ef3fa7b202b2ab77d84d9bc8ccdf969f3647cd
Stéphane Le Dorze <stephane.ledorze@gmail.com>
2018-11-21 16:15:36 +0100

    add missing changes (fixes #66)

--- src/Index/NonEmptyArray.ts
+++ src/Index/NonEmptyArray.ts
@@ -1,6 +1,6 @@
-import { Index, Optional } from '../index';
-import { NonEmptyArray } from 'fp-ts/lib/NonEmptyArray';
+import { Index, Optional } from '../index'
+import { NonEmptyArray } from 'fp-ts/lib/NonEmptyArray'
 
 export function indexNonEmptyArray<A = never>(): Index<NonEmptyArray<A>, number, A> {
-  return new Index(i => new Optional(s => s.index(i), a => s => s.updateAt(i, a).getOrElse(s)));
+  return new Index(i => new Optional(s => s.index(i), a => s => s.updateAt(i, a).getOrElse(s)))
 }
--- test/Index.ts
+++ test/Index.ts
@@ -1,8 +1,10 @@
 import { indexArray } from '../src/Index/Array'
+import { indexNonEmptyArray } from '../src/Index/NonEmptyArray'
 import { indexStrMap } from '../src/Index/StrMap'
 import * as assert from 'assert'
-import { some } from 'fp-ts/lib/Option'
+import { some, none } from 'fp-ts/lib/Option'
 import * as SM from 'fp-ts/lib/StrMap'
+import { NonEmptyArray } from 'fp-ts/lib/NonEmptyArray'
 
 describe('Index', () => {
   describe('indexStrMap', () => {
@@ -50,6 +52,49 @@ describe('Index', () => {
       const newArr = index.set('new')(arr)
 
       assert.deepEqual(newArr, arr)
+    })
+  })
+
+  describe('indexArray', () => {
+    const atOne = indexArray<string>().index(1)
+    it('get', () => {
+      assert.deepEqual(atOne.getOption(['a']), none)
+      assert.deepEqual(atOne.getOption(['a', 'b']), some('b'))
+    })
+    it('get', () => {
+      assert.deepEqual(atOne.set('x')(['a']), ['a'])
+      assert.deepEqual(atOne.set('x')(['a', 'b']), ['a', 'x'])
+    })
+    it('modify', () => {
+      assert.deepEqual(atOne.modify(v => `${v}X`)(['a']), ['a'])
+      assert.deepEqual(atOne.modify(v => `${v}X`)(['a', 'b']), ['a', 'bX'])
+    })
+    it('modifyOption', () => {
+      assert.deepEqual(atOne.modifyOption(v => `${v}X`)(['a']), none)
+      assert.deepEqual(atOne.modifyOption(v => `${v}X`)(['a', 'b']), some(['a', 'bX']))
+    })
+  })
+
+  describe('indexNonEmptyArray', () => {
+    const atOne = indexNonEmptyArray<string>().index(1)
+    it('get', () => {
+      assert.deepEqual(atOne.getOption(new NonEmptyArray('a', [])), none)
+      assert.deepEqual(atOne.getOption(new NonEmptyArray('a', ['b'])), some('b'))
+    })
+    it('get', () => {
+      assert.deepEqual(atOne.set('x')(new NonEmptyArray('a', [])), new NonEmptyArray('a', []))
+      assert.deepEqual(atOne.set('x')(new NonEmptyArray('a', ['b'])), new NonEmptyArray('a', ['x']))
+    })
+    it('modify', () => {
+      assert.deepEqual(atOne.modify(v => `${v}X`)(new NonEmptyArray('a', [])), new NonEmptyArray('a', []))
+      assert.deepEqual(atOne.modify(v => `${v}X`)(new NonEmptyArray('a', ['b'])), new NonEmptyArray('a', ['bX']))
+    })
+    it('modifyOption', () => {
+      assert.deepEqual(atOne.modifyOption(v => `${v}X`)(new NonEmptyArray('a', [])), none)
+      assert.deepEqual(
+        atOne.modifyOption(v => `${v}X`)(new NonEmptyArray('a', ['b'])),
+        some(new NonEmptyArray('a', ['bX']))
+      )
     })
   })
 })
