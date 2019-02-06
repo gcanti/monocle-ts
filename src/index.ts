f880e737a1fbe92308575d489e95f32a5c6dab78
Stéphane Le Dorze <stephane.ledorze@gmail.com>
2019-02-06 16:08:32 +0100

    use non spread object assignment

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ import { identity as id } from 'fp-ts/lib/Identity'
 import { Const, getApplicative } from 'fp-ts/lib/Const'
 
 const update = <O, K extends keyof O, A extends O[K]>(o: O, k: K, a: A): O => {
-  return a === o[k] ? o : { ...o, [k]: a }
+  return a === o[k] ? o : Object.assign({}, o, { [k]: a })
 }
 
 /*
