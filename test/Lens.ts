a0f99cef17cf2277ab3ab334eb84fc3ef47e80c9
Stéphane Le Dorze <stephane.ledorze@gmail.com>
2019-02-06 17:14:49 +0100

    add setter test for Lense structure sharing
    add structure sharing test for 'fromNullableProp'

--- test/Lens.ts
+++ test/Lens.ts
@@ -50,11 +50,13 @@ describe('Lens', () => {
     const name1 = Lens.fromProp<Person, 'name'>('name')
     assert.strictEqual(name1.get(person), 'giulio')
     assert.strictEqual(name1.modify(capitalize)(person).name, 'Giulio')
+    assert.strictEqual(name1.set('giulio')(person), person)
     assert.strictEqual(name1.modify(identity)(person), person)
 
     const name2 = Lens.fromProp<Person>()('name')
     assert.strictEqual(name2.get(person), 'giulio')
     assert.strictEqual(name2.modify(capitalize)(person).name, 'Giulio')
+    assert.strictEqual(name2.set('giulio')(person), person)
     assert.strictEqual(name2.modify(identity)(person), person)
   })
 
@@ -66,10 +68,12 @@ describe('Lens', () => {
       'name'
     ])
     assert.strictEqual(lens1.modify(capitalize)(employee).company.address.street.name, 'High street')
+    assert.strictEqual(lens1.set('high street')(employee), employee)
     assert.strictEqual(lens1.modify(identity)(employee), employee)
 
     const lens2 = Lens.fromPath<Employee>()(['company', 'address', 'street', 'name'])
     assert.strictEqual(lens2.modify(capitalize)(employee).company.address.street.name, 'High street')
+    assert.strictEqual(lens2.set('high street')(employee), employee)
     assert.strictEqual(lens2.modify(identity)(employee), employee)
   })
 
@@ -92,6 +96,7 @@ describe('Lens', () => {
     assert.strictEqual(lens1.get({}), 0)
     assert.deepStrictEqual(lens1.set(1)({ inner: { value: 1, foo: 'bar' } }), { inner: { value: 1, foo: 'bar' } })
     assert.strictEqual(lens1.get({ inner: { value: 1, foo: 'bar' } }), 1)
+    assert.strictEqual(lens1.set(1)(outer1), outer1)
     assert.strictEqual(lens1.modify(identity)(outer1), outer1)
 
     const inner2 = Lens.fromNullableProp<Outer>()('inner', { value: 0, foo: 'foo' })
@@ -100,6 +105,7 @@ describe('Lens', () => {
     assert.strictEqual(lens2.get({}), 0)
     assert.deepStrictEqual(lens2.set(1)({ inner: { value: 1, foo: 'bar' } }), { inner: { value: 1, foo: 'bar' } })
     assert.strictEqual(lens2.get({ inner: { value: 1, foo: 'bar' } }), 1)
+    assert.strictEqual(lens2.set(1)(outer1), outer1)
     assert.strictEqual(lens2.modify(identity)(outer1), outer1)
   })
 
@@ -107,6 +113,7 @@ describe('Lens', () => {
     const lens = Lens.fromProps<Person>()(['name', 'age'])
     assert.deepStrictEqual(lens.get(person), { name: 'giulio', age: 44 })
     assert.deepStrictEqual(lens.set({ name: 'Guido', age: 47 })(person), { name: 'Guido', age: 47, rememberMe: true })
+    assert.strictEqual(lens.set({ age: 44, name: 'giulio' })(person), person)
     assert.strictEqual(lens.modify(identity)(person), person)
   })
 
@@ -131,10 +138,12 @@ describe('Lens', () => {
     }
     assert.strictEqual(composition1.get(address), 'name')
     assert.deepStrictEqual(composition1.set('name2')(address), expected)
+    assert.strictEqual(composition1.set('name')(address), address)
     assert.strictEqual(composition1.modify(identity)(address), address)
 
     assert.strictEqual(composition2.get(address), composition1.get(address))
     assert.deepStrictEqual(composition2.set('name2')(address), composition1.set('name2')(address))
+    assert.strictEqual(composition2.set('name')(address), address)
     assert.strictEqual(composition2.modify(identity)(address), address)
   })
 })
