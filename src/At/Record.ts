e5e535a5d7bd31b93ff3d36081f482e1d464d64e
Malte Legenhausen <mlegenhausen@gmail.com>
2019-03-06 14:37:35 +0100

    Record lenses added

---
+++ src/At/Record.ts
@@ -0,0 +1,7 @@
+import { At, Lens } from '../index'
+import { Option } from 'fp-ts/lib/Option'
+import * as R from 'fp-ts/lib/Record'
+
+export function atRecord<A = never>(): At<Record<string, A>, string, Option<A>> {
+  return new At(at => new Lens(s => R.lookup(at, s), a => s => a.fold(R.remove(at, s), x => R.insert(at, x, s))))
+}
