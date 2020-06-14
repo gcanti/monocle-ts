96bc86cb0ca6da63c5ff9888fb5ffbdcd5652827
gcanti <giulio.canti@gmail.com>
2020-06-14 08:20:19 +0200

    move old dtslint tests

--- dtslint/ts3.5/Index_.ts
+++
@@ -1,9 +0,0 @@
-import * as RA from '../../src/Index/ReadonlyArray'
-import * as RNEA from '../../src/Index/ReadonlyNonEmptyArray'
-import * as RR from '../../src/Index/ReadonlyRecord'
-
-RA.indexReadonlyArray<string>().index(0) // $ExpectType Optional<readonly string[], string>
-
-RNEA.indexReadonlyNonEmptyArray<string>().index(0) // $ExpectType Optional<ReadonlyNonEmptyArray<string>, string>
-
-RR.indexReadonlyRecord<string>().index('a') // $ExpectType Optional<Readonly<Record<string, string>>, string>
